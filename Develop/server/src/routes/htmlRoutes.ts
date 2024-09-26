7c3e493d98266599e4a1dc0ae91e01533ac1c7f2
Emily Gretsky <emilygretsky@emilys-mbp.mynetworksettings.com>
2024-09-26 18:38:50 -0400

    Fixing getWeather Error

--- Develop/server/src/routes/htmlRoutes.ts
+++ Develop/server/src/routes/htmlRoutes.ts
@@ -6,5 +6,10 @@ const __dirname = path.dirname(__filename);
 const router = Router();
 
 // TODO: Define route to serve index.html
+// @ts-ignore
+router.get('/', (_req: Request, res: Response) => {
+    //@ts-ignore
+    res.sendFile(path.join(__dirname, '../../../client/dist/index.html'));
+});
 
 export default router;
--- Develop/server/src/service/weatherService.ts
+++ Develop/server/src/service/weatherService.ts
@@ -1,31 +1,165 @@
 import dotenv from 'dotenv';
 dotenv.config();
+import dayjs from 'dayjs';
 
 // TODO: Define an interface for the Coordinates object
+interface Coordinates {
+  name: string;
+  lat: number;
+  lon: number;
+  country?: string;
+}
 
 // TODO: Define a class for the Weather object
+class Weather {
+  city: string;
+  date: string;
+  tempF: string;
+  windSpeed: string;
+  humidity: string;
+  icon: string;
+  iconDescription: string;
+
+  constructor (city: string, date: string, tempF: string, windSpeed: string, humidity: string, icon: string, iconDescription: string) {
+    this.city = city;
+    this.date = date;
+    this.icon = icon;
+    this.iconDescription = iconDescription;
+    this.tempF = tempF;
+    this.windSpeed = windSpeed;
+    this.humidity = humidity;
+  }
+}
 
 // TODO: Complete the WeatherService class
 class WeatherService {
   // TODO: Define the baseURL, API key, and city name properties
+  private baseURL?: string;
+  private API_KEY?: string;
+  private city = '';
+
+  constructor () {
+    this.baseURL = process.env.API_BASE_URL || '';
+    this.API_KEY = process.env.API_KEY || '';
+  }
+
   // TODO: Create fetchLocationData method
-  // private async fetchLocationData(query: string) {}
-  // TODO: Create destructureLocationData method
-  // private destructureLocationData(locationData: Coordinates): Coordinates {}
-  // TODO: Create buildGeocodeQuery method
-  // private buildGeocodeQuery(): string {}
-  // TODO: Create buildWeatherQuery method
-  // private buildWeatherQuery(coordinates: Coordinates): string {}
-  // TODO: Create fetchAndDestructureLocationData method
-  // private async fetchAndDestructureLocationData() {}
-  // TODO: Create fetchWeatherData method
-  // private async fetchWeatherData(coordinates: Coordinates) {}
-  // TODO: Build parseCurrentWeather method
-  // private parseCurrentWeather(response: any) {}
-  // TODO: Complete buildForecastArray method
-  // private buildForecastArray(currentWeather: Weather, weatherData: any[]) {}
-  // TODO: Complete getWeatherForCity method
-  // async getWeatherForCity(city: string) {}
+  private async fetchLocationData(query: string) {
+    try {
+        const response: Coordinates[] = await fetch(query).then((res) =>
+          res.json()
+        );
+        return response[0];
+    } catch (error: any) {
+      console.error(error);
+      return error;
+    }
+  }
+// TODO: Create destructureLocationData method
+private destructureLocationData(locationData: Coordinates) {
+  const { name, lat, lon } = locationData;
+  const coordinates: Coordinates = {
+    name,
+    lat,
+    lon,
+  };
+  return coordinates;
 }
+
+// TODO: Create buildGeocodeQuery method
+private buildGeocodeQuery(): string {
+  const geocodeQuery = `${this.baseURL}/geo/1.0/direct?q=${this.city}&limit=1&appid=${this.API_KEY}`;
+  return geocodeQuery;
+}
+
+// TODO: Create buildWeatherQuery method
+private buildWeatherQuery(coordinates: Coordinates): string {
+  const buildWeatherQuery = `${this.baseURL}/data/2.5/forecast?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${this.API_KEY}`;
+  return buildWeatherQuery;
+}
+
+// TODO: Create fetchAndDestructureLocationData method
+private async fetchAndDestructureLocationData() {
+  return await this.fetchLocationData(this.buildGeocodeQuery()).then((data) =>
+    this.destructureLocationData(data)
+  );
+}
+
+// TODO: Create fetchWeatherData method
+private async fetchWeatherData(coordinates: Coordinates) {
+  try {
+    const response = await fetch(this.buildWeatherQuery(coordinates)).then(
+      (res) => res.json()
+    );
+    if (!response) {
+      throw new Error('Weather data not found');
+    }
+    const currentWeather: Weather = this.parseCurrentWeather(
+      response.list[0]
+    );
+    const forecast: Weather[] = this.buildForecastArray(
+      currentWeather,
+      response.list
+    );
+    return forecast;
+  } catch (error: any) {
+    console.error(error);
+    return error;
+  }
+
+}
+// // TODO: Build parseCurrentWeather method
+private parseCurrentWeather(response: any) {
+  const parsedDate = dayjs.unix(response.dt).format('M/D/YYYY');
+  const currentWeather = new Weather(
+    this.city,
+    parsedDate,
+    response.main.temp,
+    response.wind.speed,
+    response.main.humidity,
+    response.weather[0].icon,
+    response.weather[0].description || response.weather[0].main
+  );
+  return currentWeather;
+}
+
+// TODO: Complete buildForecastArray method
+private buildForecastArray(currentWeather: Weather, weatherData: any[]) {
+  const weatherForecast: Weather[] = [currentWeather];
+  const filteredWeatherData = weatherData.filter((data: any) => {
+    return data.dt_txt.includes('12:00:00');
+  });
+  for (const day of filteredWeatherData) {
+    weatherForecast.push(
+      new Weather(
+        this.city,
+        dayjs.unix(day.dt).format('M/D/YYYY'),
+        day.main.temp,
+        day.wind.speed,
+        day.main.humidity,
+        day.weather[0].icon,
+        day.weather[0].description || day.weather[0].main
+      )
+    );
+  }
+  return weatherForecast;
+}
+
+// TODO: Complete getWeatherForCity method
+async getWeatherForCity(city: string) {
+  try {
+    this.city = city;
+    const coordinates = await this.fetchAndDestructureLocationData();
+    if (coordinates) {
+      const weather = await this.fetchWeatherData(coordinates);
+      return weather;
+    }
+    throw new Error('Weather data not found');
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
+};
 
 export default new WeatherService();
